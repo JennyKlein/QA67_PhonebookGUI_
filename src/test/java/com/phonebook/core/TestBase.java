a3dbf000640fd1b903febb488f0663a4b7100573
JennyKlein <jenny.klein@mail.de>
2026-04-27 09:09:47 +0200

    Add logback 2

--- src/test/java/com/phonebook/core/TestBase.java
+++ src/test/java/com/phonebook/core/TestBase.java
@@ -3,10 +3,13 @@ package com.phonebook.core;
 import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
@@ -30,13 +33,20 @@ public class TestBase {
          }
 
     @BeforeMethod
-    public void startTest(){
-        logger.info("Start test");
+    public void startTest(Method method){
+        logger.info("Start test{}", method.getName());
             }
 
      @AfterMethod
-    public void stopTet(){
+    public void stopTest(ITestResult result){
+        if (result.isSuccess()){
+            logger.info("PASSED: {}",result.getMethod().getMethodName());
+        }else{
+            logger.error("FAILED: {}. Screenshot - > {}",result.getMethod().getMethodName(),
+                    app.getUser().takeScreenshot());
+        }
         logger.info("Stop test");
+        logger.info("*****************************************");
             }
 
 }
